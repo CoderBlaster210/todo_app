68e4b5630ef2e7acdd6fbd7b7ead91e4453aa2c9
Zareeb Chowdhury <zareeb.chowdhury234@gmail.com>
2018-05-12 15:25:22 +0000

    Added Variables

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,6 +1,11 @@
 class TodoController < ApplicationController
     def index
+        @todo_count = 4
+        @todo_title =  "Workshop Todo List"
+        @todo_time_measure = "Pomodoro(s)"
     end
     def show
+        @todo_title = "Workshop Todo List"
     end
+    
 end
